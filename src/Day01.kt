8d70bd52dd18cdf08525c364124c6c1da3282257
Toni Naumanen <toni.naumis@gmail.com>
2023-12-10 17:59:27 +0200

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,55 @@
 fun main() {
+    val spelledToDigit = mapOf(
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9',
+    )
+
+    fun String.firstDigit(): Char {
+        var string = ""
+        for (char in this) {
+            string += char
+            for (digit in spelledToDigit) {
+                if (string.contains(digit.value) || string.contains(digit.key)) {
+                    return digit.value
+                }
+            }
+        }
+        throw Exception("no digit found in string $string")
+    }
+
+    fun String.lastDigit(): Char {
+        var string = ""
+        for (char in this.reversed()) {
+            string = "$char$string"
+            for (digit in spelledToDigit) {
+                if (string.contains(digit.value) || string.contains(digit.key)) {
+                    return digit.value
+                }
+            }
+        }
+        throw Exception("no digit found in string $string")
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstDigit = line.first { it.isDigit() }
+            val lastDigit = line.last { it.isDigit() }
+            "${firstDigit}${lastDigit}".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line -> "${line.firstDigit()}${line.lastDigit()}".toInt() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
 }
