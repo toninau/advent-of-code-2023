2fc7d322812b3ba36c1df037f78aa7e65368accc
Toni Naumanen <toni.naumis@gmail.com>
2023-12-24 16:51:15 +0200

    Add day 3 part 2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,29 +1,28 @@
 fun main() {
-    fun hasAdjacentSymbol(
-        charPositionY: Int,
-        charStartPositionX: Int,
-        charEndPositionX: Int,
+    fun findPointAdjacentNumbers(
+        point: Point,
         schematic: List<List<Char>>,
-    ): Boolean {
-        val lineLength = schematic[charPositionY].size - 1
-        val startRangeY = if (charPositionY - 1 > 0) charPositionY - 1 else 0
-        val endRangeY = if (charPositionY + 1 < schematic.size) charPositionY + 1 else schematic.size - 1
-        val startRangeX = if (charStartPositionX - 1 > 0) charStartPositionX - 1 else 0
-        val endRangeX = if (charEndPositionX + 1 < lineLength) charEndPositionX + 1 else lineLength - 1
+        numbers: Map<Point, FoundNumber>
+    ): Set<FoundNumber> {
+        val lineLength = schematic[point.y].size - 1
+        val startRangeY = if (point.y - 1 > 0) point.y - 1 else 0
+        val endRangeY = if (point.y + 1 < schematic.size) point.y + 1 else schematic.size - 1
+        val startRangeX = if (point.x - 1 > 0) point.x - 1 else 0
+        val endRangeX = if (point.x + 1 < lineLength) point.x + 1 else lineLength - 1
+        val points = mutableSetOf<FoundNumber>()
 
-        for (i in startRangeY..endRangeY) {
-            for (j in startRangeX..endRangeX) {
-                if (!schematic[i][j].isDigit() && schematic[i][j] != '.') {
-                    return true
+        for (y in startRangeY..endRangeY) {
+            for (x in startRangeX..endRangeX) {
+                if (schematic[y][x].isDigit()) {
+                    numbers[Point(x, y)]?.let { points.add(it) }
                 }
             }
         }
-        return false
+        return points.toSet()
     }
 
-    fun part1(input: List<String>): Int {
-        val schematic = input.map { it.toList() } // [y], [x]
-        val foundNumbers = mutableListOf<Int>()
+    fun findAllNumbers(schematic: List<List<Char>>): Map<Point, FoundNumber> {
+        val foundNumbers = mutableMapOf<Point, FoundNumber>()
         schematic.forEachIndexed { y, line ->
             var number = ""
             var charStartPositionX = 0
@@ -35,18 +34,52 @@ fun main() {
                     number += character
                 }
                 if ((x == line.size - 1 && number != "") || (!character.isDigit() && number != "")) {
-                    if (hasAdjacentSymbol(y, charStartPositionX, x - 1, schematic)) {
-                        foundNumbers.add(number.toInt())
-                    }
+                    val foundNumber = FoundNumber("$y$charStartPositionX".toInt(), number.toInt())
+                    foundNumbers[Point(charStartPositionX, y)] = foundNumber
+                    foundNumbers[Point(x - 1, y)] = foundNumber
                     number = ""
                     charStartPositionX = 0
                 }
 
             }
         }
-        return foundNumbers.sum()
+        return foundNumbers.toMap()
     }
 
-    val input = readInput("Day03")
-    println(part1(input))
+    fun part1(schematic: List<List<Char>>, allNumbers: Map<Point, FoundNumber>): Int {
+        val numbersAdjacentToSymbol = mutableSetOf<FoundNumber>()
+
+        schematic.forEachIndexed { y, line ->
+            line.forEachIndexed { x, character ->
+                if (!character.isDigit() && character != '.') {
+                    numbersAdjacentToSymbol.addAll(findPointAdjacentNumbers(Point(x, y), schematic, allNumbers))
+                }
+            }
+        }
+        return numbersAdjacentToSymbol.sumOf { it.number }
+    }
+
+    fun part2(schematic: List<List<Char>>, allNumbers: Map<Point, FoundNumber>): Int {
+        val gearRatios = mutableListOf<Int>()
+
+        schematic.forEachIndexed { y, line ->
+            line.forEachIndexed { x, character ->
+                if (character == '*') {
+                    val adjacentNumbers = findPointAdjacentNumbers(Point(x, y), schematic, allNumbers)
+                    if (adjacentNumbers.size == 2) {
+                        gearRatios.add(adjacentNumbers.fold(1) { sum, element -> sum * element.number })
+                    }
+                }
+            }
+        }
+        return gearRatios.sum()
+    }
+
+    val schematic = readInput("Day03").map { it.toList() } // [y][x]
+    val allNumbers = findAllNumbers(schematic)
+    println(part1(schematic, allNumbers))
+    println(part2(schematic, allNumbers))
 }
+
+data class FoundNumber(val id: Int, val number: Int)
+data class Point(val x: Int, val y: Int)
