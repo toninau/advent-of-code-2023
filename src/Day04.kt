b3ce2cf2b4184beb2342dd62233b54351646722b
Toni Naumanen <toni.naumis@gmail.com>
2023-12-25 12:49:15 +0200

    Add day 4 part 2 solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,14 +1,14 @@
 fun main() {
-    data class Scratchcard(val winningNumbers: List<Int>, val guessedNumbers: List<Int>)
+    data class Scratchcard(val index: Int, val winningNumbers: List<Int>, val guessedNumbers: List<Int>)
 
     fun parse(input: List<String>): List<Scratchcard> {
-        return input.map { line ->
+        return input.mapIndexed { index, line ->
             val scratchcardNumbers = line.substringAfter(':').split('|').map { numbers ->
                 numbers.trim().split(' ')
                     .filter { it.isNotBlank() }
                     .map { it.toInt() }
             }
-            Scratchcard(winningNumbers = scratchcardNumbers[0], guessedNumbers = scratchcardNumbers[1])
+            Scratchcard(index = index, winningNumbers = scratchcardNumbers[0], guessedNumbers = scratchcardNumbers[1])
         }
     }
 
@@ -20,7 +20,18 @@ fun main() {
         }
     }
 
+    fun part2(scratchcards: List<Scratchcard>): Int {
+        val deque = ArrayDeque(scratchcards)
+        deque.forEach {
+            val wonScratchcardsAmount = it.winningNumbers.intersect(it.guessedNumbers.toSet()).size
+            val wonScratchcards = scratchcards.slice(it.index + 1..it.index + wonScratchcardsAmount)
+            deque.addAll(wonScratchcards)
+        }
+        return deque.size
+    }
+
     val input = readInput("Day04")
     val scratchcards = parse(input)
     println(part1(scratchcards))
+    println(part2(scratchcards))
 }
